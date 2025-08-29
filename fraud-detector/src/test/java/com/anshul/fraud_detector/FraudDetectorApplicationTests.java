e25201c13e5a9cf1cc6dc7d860bfc32df157dd8c
Anshul <anshulbhaskar50@gmail.com>
2025-08-29 23:25:15 +0530

    Spring Setup

---
+++ fraud-detector/src/test/java/com/anshul/fraud_detector/FraudDetectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anshul.fraud_detector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudDetectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
