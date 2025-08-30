712542659f47d7b26cd73f24994fed01b3e12059
Anshul <anshulbhaskar50@gmail.com>
2025-08-30 12:09:06 +0530

    Entities

--- fraud-detector/src/main/java/com/anshul/fraud_detector/domain/Transaction.java
+++ fraud-detector/src/main/java/com/anshul/fraud_detector/domain/Transaction.java
@@ -1,4 +1,28 @@
 package com.anshul.fraud_detector.domain;
 
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.Instant;
+
+@Entity @Table(name="transaction")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor @Builder
 public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long transactionId;
+
+    @ManyToOne(fetch=FetchType.LAZY) @JoinColumn(name = "user_id")
+    private User user;
+
+    private Double amount;
+    private String location;       // txn city
+    private String deviceId;
+    private Instant timestamp;
+
+    private Boolean fraudFlag;
+    private String fraudReason;    // short reason
+    private Double fraudScore;     // batch ML-like score (0..1)
 }
--- fraud-detector/src/main/java/com/anshul/fraud_detector/domain/User.java
+++ fraud-detector/src/main/java/com/anshul/fraud_detector/domain/User.java
@@ -1,7 +1,12 @@
 package com.anshul.fraud_detector.domain;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity @Table(name= "users" )
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
 public class User {
     @Id
     private String userId;
-    
+    private String city;
 }
